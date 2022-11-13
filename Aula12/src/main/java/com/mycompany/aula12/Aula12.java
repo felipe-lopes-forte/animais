f5a9633ae823b39cb7af572a157141193d2c7dc0
Felipe Forte <felipezforte@gmail.com>
2022-11-13 12:32:16 -0300

    criação do projeto

---
+++ Aula12/src/main/java/com/mycompany/aula12/Aula12.java
@@ -0,0 +1,20 @@
+package com.mycompany.aula12;
+public class Aula12 {
+    public static void main(String[] args) {
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+        Canguru c = new Canguru();
+     ;  Cachorro k = new Cachorro();
+        Cobra j = new Cobra();
+        Tartaruga t = new Tartaruga();
+        Goldfish g = new Goldfish();
+        Arara e = new Arara();
+        
+        c.locomover();
+        c.emitirSom();
+        k.emitirSom();
+
+    }
+}
